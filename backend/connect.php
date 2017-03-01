925079ca8714174658c8ec4d63218c260d144448
Nick Sw <swaggarnick@gmail.com>
2017-02-28 17:53:30 -0800

    sql

--- backend/connect.php
+++ backend/connect.php
@@ -1,6 +1,14 @@
 <?php 
 
-	$dbcon = mysqli_connect("localhost", "root", "", "armack") or die("error");
+	//$dbcon = mysqli_connect("localhost", "root", "", "armack") or die("error");
 
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$dbcon = new mysqli($server, $username, $password, $db);
 
  ?>
